082f5659ea33522c7903775498bf204acb00fd01
bdream <bdletterbox@gmail.com>
2022-05-15 22:44:26 +0300

    Update viziers calculation related to selected djinn.

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const { createApp } = Vue
 
 const defaultPalmTreeVPs = 3;
+const defaultVizierVPs = 1;
 
 const app = createApp({
     data() {
         return {
             goldCount: 0,
             viziersCount: 0,
+            vizierVPs: defaultVizierVPs,
+            viziersScore: 0,
             eldersCount: 0,
             djinniesScore: 0,
             palmTreesCount: 0,
@@ -28,7 +31,7 @@ const app = createApp({
                 {title: "Hagis (10)", key: {name: "Hagis", value: 10}},
                 {title: "Haurvatat (8) [Each of Palm Trees is worth 5 VPs instead of 3]", key: {name: "Haurvatat", value: 8}},
                 {title: "Iblis (8)", key: {name: "Iblis", value: 8}},
-                {title: "Jafaar (6)", key: {name: "Jafaar", value: 6}},
+                {title: "Jafaar (6) [Each Vizier is worth 3 VPs instead of 1", key: {name: "Jafaar", value: 6}},
                 {title: "Kandicha (6)", key: {name: "Kandicha", value: 6}},
                 {title: "Kumarbi (6)", key: {name: "Kumarbi", value: 6}},
                 {title: "Lamia (10)", key: {name: "Lamia", value: 10}},
@@ -48,8 +51,12 @@ const app = createApp({
         scorePalmTrees() {
             this.palmTreesScore = this.palmTreesCount * this.palmTreeVPs;
         },
+        scoreViziers() {
+            this.viziersScore = this.viziersCount * this.vizierVPs;
+        },
         scoreDjinns() {
             this.palmTreeVPs = defaultPalmTreeVPs;
+            this.vizierVPs = defaultVizierVPs;
 
             var score = 0;
             for(var djinnKey in this.selectedDjinns) {
@@ -60,6 +67,9 @@ const app = createApp({
                     this.palmTreeVPs = 5;
                 }
 
+                if (djinn.name === "Jafaar") {
+                    this.vizierVPs = 3;
+                }
             }
 
             this.djinniesScore = score;
@@ -67,9 +77,10 @@ const app = createApp({
         scoreTotal() {
             this.scoreDjinns();
             this.scorePalmTrees();
+            this.scoreViziers();
 
             this.totalScore = parseInt(this.goldCount)
-                + parseInt(this.viziersCount)
+                + parseInt(this.viziersScore)
                 + parseInt(this.eldersCount * 2)
                 + parseInt(this.djinniesScore)
                 + parseInt(this.palmTreesScore)
