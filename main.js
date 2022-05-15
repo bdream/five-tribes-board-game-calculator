0bca9983c2ce15523ff4f9d98fc5c9694c8173d7
bdream <bdletterbox@gmail.com>
2022-05-15 19:10:09 +0300

    Logic was moved to main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue
+
+const app = createApp({
+    data() {
+        return {
+            message: 'Five Tribes Board Game Calculator',
+            goldCount: 0,
+            viziersCount: 0,
+            eldersCount: 0,
+            djinniesScore: 0,
+            palmTreesCount: 0,
+            palacesCount: 0,
+            camelsScore: 0,
+            resourceCardsScore: 0,
+            totalScore: 0
+        }
+    },
+    methods: {
+        scoreTotal() {
+            this.totalScore = parseInt(this.goldCount)
+                + parseInt(this.viziersCount)
+                + parseInt(this.eldersCount * 2)
+                + parseInt(this.djinniesScore)
+                + parseInt(this.palmTreesCount * 3)
+                + parseInt(this.palacesCount * 5)
+                + parseInt(this.camelsScore)
+                + parseInt(this.resourceCardsScore);
+        }
+    }
+})
+
+app.use(BalmUI);
+app.use(BalmUIPlus);
+
+app.mount('#app');
